3a25613d8a8f7999de1481e5fef7ab3a61cde39e
Mark Cotner <Mark.Cotner@coxautoinc.com>
2025-12-02 11:42:12 -0500

    PR from arctarus, release 1.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lather.MixProject do
   def project do
     [
       app: :lather,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description:
@@ -72,8 +72,8 @@ defmodule Lather.MixProject do
     [
       main: "readme",
       name: "Lather SOAP Library",
-      source_ref: "v1.0.1",
-      source_url: "https://github.com/markcotner/lather",
+      source_ref: "v1.0.2",
+      source_url: "https://github.com/awksedgreep/lather",
       extras: [
         "README.md",
         "CHANGELOG.md",
